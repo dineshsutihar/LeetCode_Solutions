69f8df33dd8464fdb7daa82e64fd591b17859083
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-02-11 13:39:49 +0530

    Time: 0 ms (100.00%), Space: 45.5 MB (23.14%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,35 @@
+// My method
+
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         Arrays.sort(nums);
+//         int len=nums.length +1;
+//         int[] numss = new int[len];
+//         System.arraycopy(nums,0,numss,0,len-1);
+        
+//         for(int i=0; i<len; i++){
+//             if(i!=numss[i] ){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+// alternative method
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int len=nums.length +1;
-        int[] numss = new int[len];
-        System.arraycopy(nums,0,numss,0,len-1);
-        
-        for(int i=0; i<len; i++){
-            if(i!=numss[i] ){
-                return i;
-            }
-        }
-        return -1;
+       int length = nums.length;
+       int sum = 0;
+       int actualsum = 0;
+       for(int i = 0; i <= nums.length; i++){
+           sum += i;
+       }
+       for(int i = 0; i < nums.length; i++){
+           actualsum += nums[i];
+       }
+       return sum - actualsum;
     }
 }
