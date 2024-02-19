bf56bdc64e0f2910b49a4a36bf68d3f3205e0c8e
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-02-19 18:13:26 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (56.93%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        
+        //here we are checking weather the number is greater than 0 and binary AND operation on n & n-1 which always return 0
         return n>0 && (n&(n-1)) ==0;
     }
 }
