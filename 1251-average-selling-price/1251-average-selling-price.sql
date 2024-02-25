32f41f6b0b229a7856d1bd9bab339c71192a58d5
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-02-25 23:48:11 +0530

    Time: 3820 ms (5.11%), Space: 0B (100.00%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,6 @@
+SELECT
+    p.product_id,
+    NVL(ROUND(SUM(p.price * u.units) / SUM(u.units), 2),0) AS average_price FROM Prices p
+Left JOIN UnitsSold u ON p.product_id = u.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
