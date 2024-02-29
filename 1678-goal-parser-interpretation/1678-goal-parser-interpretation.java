0461a499eca48e0c7fa53cd2aeb97fb98a1499ab
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-02-29 23:57:45 +0530

    Time: 1 ms (72.79%), Space: 41.3 MB (78.28%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,7 +1,5 @@
 class Solution {
     public String interpret(String command) {
-        command.replace("()","o");
-        System.out.println(command.replace("()","o").replace("(al)","al"));
         return command.replace("()","o").replace("(al)","al");
     }
 }
