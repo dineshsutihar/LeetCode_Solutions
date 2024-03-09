9d92cb86e6bd0cee6718901b376e697b312b6da4
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-03-09 08:13:07 +0530

    Time: 2 ms (81.56%), Space: 62.6 MB (47.63%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,16 +1,36 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        Set<Integer> val = new HashSet<>();
+        
+        //Orignal Approach i have used 
+        
+       //  Set<Integer> val = new HashSet<>();
         int len1 = nums1.length;
         int len2 = nums2.length;
-       for(int num: nums1){
-           val.add(num);
-       }
-        for(int num: nums2){
-           if( val.contains(num)){
-               return num;
-           }
+       // for(int num: nums1){
+       //     val.add(num);
+       // }
+       //  for(int num: nums2){
+       //     if( val.contains(num)){
+       //         return num;
+       //     }
+       //  }
+       //  return -1;
+        
+        //alternating approach using 2 pointer approach 
+        
+        int i =0, j=0;
+        
+        while(i<len1 && j<len2){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }else if(nums1[i]<nums2[j]){
+                i++;
+            }else {
+                j++;
+            }
+            
         }
         return -1;
+        
     }
 }
