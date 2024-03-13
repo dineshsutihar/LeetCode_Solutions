5bbd72a32a56655229748364015a82dbc8e41bfd
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-03-13 23:06:34 +0530

    Time: 1 ms (77.54%), Space: 40.5 MB (67.67%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,21 +1,43 @@
 class Solution {
     public int pivotInteger(int n) {
         
-        for(int i=1; i<=n; i++){
-            int sumLeft = 0; 
-            int sumRight = 0; 
+//         for(int i=1; i<=n; i++){
+//             int sumLeft = 0; 
+//             int sumRight = 0; 
             
-            for(int j = 1 ; j<=i; j++){
-                sumLeft +=j; 
+//             for(int j = 1 ; j<=i; j++){
+//                 sumLeft +=j; 
+//             }
+            
+            
+//             for(int k = i ; k<=n; k++){
+//                 sumRight +=k; 
+//             }
+            
+//             if(sumLeft == sumRight){
+//                 return i; 
+//             }
+//         }
+        
+        
+        //lets use two pointer method
+        int sumLeft =1; 
+        int sumRight =n;
+        int leftValue =1; 
+        int rightValue =n;
+        
+         if (n == 1) return n;
+        
+        
+        while(leftValue<rightValue){
+            if(sumLeft<sumRight){
+                sumLeft += ++leftValue;
+            } else{
+               sumRight += --rightValue;
             }
             
-            
-            for(int k = i ; k<=n; k++){
-                sumRight +=k; 
-            }
-            
-            if(sumLeft == sumRight){
-                return i; 
+            if(sumLeft == sumRight && leftValue + 1 == rightValue - 1){
+                return leftValue + 1;
             }
         }
         
