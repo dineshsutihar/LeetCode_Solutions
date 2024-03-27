1f894bf3bb50159b26c3c7a85239b6e43cebaa48
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-03-27 11:45:31 +0530

    Time: 4 ms (100.00%), Space: 47.6 MB (49.37%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,20 +1,46 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int count = 0; 
-        int len = nums.length;
-        for(int i =0; i<len; i++){
-            int product =  1; 
-            for(int j=i; j<len; j++){
-                // if(product>=k) break; 
-                product*=nums[j];
-                if(product<k){
-                    count++;
-                }else{
-                    break;
-                }
-            }
+        
+        
+        //BruteForce method => O(n^2)
+        // int count = 0; 
+        // int len = nums.length;
+        // for(int i =0; i<len; i++){
+        //     int product =  1; 
+        //     for(int j=i; j<len; j++){
+        //         // if(product>=k) break; 
+        //         product*=nums[j];
+        //         if(product<k){
+        //             count++;
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return count; 
+        
+        //Sliding window technique => O(n)
+        
+        if(k<=1){
+            return 0;
         }
-        return count; 
+        int n = nums.length;
+        int start =0; 
+        int end = 0; 
+        int prod =1 ;
+        int count = 0;
+        while(end<n){
+            //expansion phase 
+            prod = prod*nums[end];
+            //shringking
+            while(prod>=k){
+                prod = prod/nums[start];
+                start++;
+            }
+            count = count +(end-start+1);
+            end++;
+        }
+        return count;
         
     }
 }
