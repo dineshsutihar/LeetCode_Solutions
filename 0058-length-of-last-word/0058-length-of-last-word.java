b733219b583f82c637eafe7f750287d41ce4ad59
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-04-01 15:04:55 +0530

    Time: 1 ms (36.75%), Space: 41.7 MB (30.67%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -4,6 +4,28 @@ class Solution {
         int len = tokens.length;
         int lastWordLength = tokens[len-1].length();
         return lastWordLength ;
+        
+       
+        //Old Code
+//         // Trim the input string to remove leading and trailing whitespaces
+//         s = s.trim();
+
+//         // Check if the string is empty after trimming
+//         if (s.isEmpty()) {
+//             return 0;
+//         }
+
+//         // Find the last space in the trimmed string
+//         int lastSpaceIndex = s.lastIndexOf(' ');
+
+//         // If there are no spaces, the entire string is the last word
+//         if (lastSpaceIndex == -1) {
+//             return s.length();
+//         }
+
+//         // Return the length of the last word
+//         return s.length() - lastSpaceIndex - 1;
+
             
         
     }
