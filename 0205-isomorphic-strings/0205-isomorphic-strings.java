d0871b0eb6c32ae6fbe3824ca2c0262ebdb96148
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-04-02 17:28:06 +0530

    Time: 6 ms (93.47%), Space: 42.1 MB (89.19%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,21 +1,33 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> sToTMap = new HashMap<>();
-        HashMap<Character, Character> tToSMap = new HashMap<>();
+        // HashMap<Character, Character> sToTMap = new HashMap<>();
+        // HashMap<Character, Character> tToSMap = new HashMap<>();
+        int sToTMap[] = new int[128];
+        int tToSMap[] = new int[128];
+        
+        Arrays.fill(sToTMap,-1);
+        Arrays.fill(tToSMap,-1);
+
         StringBuilder stS = new StringBuilder(s);
         StringBuilder stT = new StringBuilder(t);
+
 
         int n = s.length();
         for(int i =0; i<n;i++){
             char s1 = stS.charAt(i);
             char t1 = stT.charAt(i);
-            if(!sToTMap.containsKey(s1)){
-                sToTMap.put(s1,t1);
+            // if(!sToTMap.containsKey(s1)){
+            //     sToTMap.put(s1,t1);
+            // }
+            if(sToTMap[s1]==-1){
+                sToTMap[s1]=t1;
             }
-            if(!tToSMap.containsKey(t1)){
-                tToSMap.put(t1,s1);
+            if(tToSMap[t1]==-1){
+                tToSMap[t1]=s1;
             }
-            if(sToTMap.get(s1)!=t1 || tToSMap.get(t1)!=s1){
+            
+           
+            if(sToTMap[s1]!=t1 || tToSMap[t1]!=s1){
                 return false; 
             }
         }
