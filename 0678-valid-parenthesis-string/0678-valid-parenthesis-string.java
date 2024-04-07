16e99244eee93d6e91af1ba48a042d9762bea664
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-04-07 23:19:21 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (30.05%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,38 +1,74 @@
 class Solution {
     public boolean checkValidString(String s) {
-        Stack<Integer> openBrackets = new Stack<>();
-        Stack<Integer> asterisks = new Stack<>();
-
-        int n = s.length();
-        for(int i=0; i<n; i++){
+        //using 2 pointer Approach 
+        
+        int openCount = 0, closeCount = 0, len = s.length()-1;
+        
+        for(int i =0; i<=len; i++){
+            
+            //Counting the open braces and astrics 
             char ch = s.charAt(i);
-            if(ch == '('){
-                openBrackets.push(i);
-            }else if(ch == '*'){
-                asterisks.push(i);
-            } else {
-                
-                
-                if(!openBrackets.empty()){
-                    openBrackets.pop();
-                }else if(!asterisks.empty()){
-                    asterisks.pop();
-                }else {
-                    return false; 
-                }
+            if(ch == '(' || ch == '*'){
+                openCount++;
+            }else {
+                openCount--;
+            }
+            
+            //Counting closing braces 
+            char ech = s.charAt(len-i);
+            if(ech == ')' || ech == '*'){
+                closeCount++;
+            }else {
+                closeCount--;
             }
             
             
-        }
-        
-        while(!openBrackets.isEmpty() && !asterisks.isEmpty()){
-            if(openBrackets.pop() > asterisks.pop()){
+            //if at any point open and close count goes negative, then the string is invalid
+            if(closeCount<0 || openCount <0){
                 return false; 
             }
         }
         
+        return true;
         
-        return openBrackets.isEmpty();
+        
+        
+        
+        
+        //Using 2 Stack Approach 
+//         Stack<Integer> openBrackets = new Stack<>();
+//         Stack<Integer> asterisks = new Stack<>();
+
+//         int n = s.length();
+//         for(int i=0; i<n; i++){
+//             char ch = s.charAt(i);
+//             if(ch == '('){
+//                 openBrackets.push(i);
+//             }else if(ch == '*'){
+//                 asterisks.push(i);
+//             } else {
+                
+                
+//                 if(!openBrackets.empty()){
+//                     openBrackets.pop();
+//                 }else if(!asterisks.empty()){
+//                     asterisks.pop();
+//                 }else {
+//                     return false; 
+//                 }
+//             }
+            
+            
+//         }
+        
+//         while(!openBrackets.isEmpty() && !asterisks.isEmpty()){
+//             if(openBrackets.pop() > asterisks.pop()){
+//                 return false; 
+//             }
+//         }
+        
+        
+//         return openBrackets.isEmpty();
         
     }
 }
