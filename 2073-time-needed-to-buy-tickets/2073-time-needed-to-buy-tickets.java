c4cd5ae32a2666746444a38ca7cfbfaf07438e0f
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-04-09 17:02:52 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (47.55%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -3,17 +3,28 @@ class Solution {
         int len = tickets.length;
         int time = 0;
         
-        //iterating over tickets
-        while(tickets[k]>0){
-            for(int i=0; i<len; i++){
-                if(tickets[i]>0 && tickets[k]>0){
-                    tickets[i]=tickets[i]-1;
-                    time++;
-                }
+        for(int i=0; i<len;i++){
+            if(i<=k){
+                time = time + Math.min(tickets[k],tickets[i]);
+            }
+            //i is greater than k
+            else{
+                time = time + Math.min(tickets[k]-1,tickets[i]);
+
             }
         }
         
+        //Old Code
         
+        // //iterating over tickets
+        // while(tickets[k]>0){
+        //     for(int i=0; i<len; i++){
+        //         if(tickets[i]>0 && tickets[k]>0){
+        //             tickets[i]=tickets[i]-1;
+        //             time++;
+        //         }
+        //     }
+        // }
         
         return time;
         
