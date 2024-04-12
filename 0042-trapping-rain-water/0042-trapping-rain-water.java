7c7f21aa9f0589626ee09732070d06543ddf5522
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-04-12 11:03:34 +0530

    Time: 1 ms (71.17%), Space: 46.6 MB (16.61%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -24,3 +24,5 @@ class Solution {
         
     }
 }
+
+//Approach: Take 2 pointers l(left pointer) and r(right pointer) pointing to 0th and (n-1)th index respectively. Take two variables leftMax and rightMax and initialize them to 0. If height[l] is less than or equal to height[r] then if leftMax is less than height[l] update leftMax to height[l] else add leftMax-height[l] to your final answer and move the l pointer to the right i.e l++. If height[r] is less than height[l], then now we are dealing with the right block. If height[r] is greater than rightMax, then update rightMax to height[r] else add rightMax-height[r] to the final answer. Now move r to the left. Repeat these steps till l and r crosses each other.
