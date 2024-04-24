a3f52e72945d913994ff687be8bef54bc36b402f
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-04-24 23:03:23 +0530

    Time: 0 ms (100.00%), Space: 40 MB (84.21%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,12 +1,31 @@
 class Solution {
     public int tribonacci(int n) {
-        int arr[] = new int[38];
-        arr[0]=0; 
-        arr[1]=1;
-        arr[2]=1;
-        for(int i =3; i<=n; i++){
-            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        // //DP approach 
+        // int arr[] = new int[38];
+        // arr[0]=0; 
+        // arr[1]=1;
+        // arr[2]=1;
+        // for(int i =3; i<=n; i++){
+        //     arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        // }
+        // return arr[n];
+        
+        
+        
+        //alternative approach without extra space; 
+        int p0 =0, p1=1, p2=1,sum=0; 
+        
+        if(n<=2){
+            return (n==0)?0:1;
         }
-        return arr[n];
+        
+        for(int i =3; i<=n; i++){
+            sum = p0+p1+p2; 
+            p0=p1; 
+            p1=p2; 
+            p2=sum; 
+        }
+        
+        return sum; 
     }
 }
