21ebf93e01f8002f77df90eb04b87b7f9c771152
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-04-25 22:55:44 +0530

    Time: 33 ms (59.52%), Space: 44.4 MB (84.13%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
@@ -1,45 +1,23 @@
 class Solution {
     public int longestIdealString(String s, int k) {
         int N = s.length();
+        int[] dp = new int[26];
 
-        // Initialize all dp values to -1 to indicate non-visited states
-        int[][] dp = new int[N][26];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
-        // Find the maximum dp[N-1][c] and return the result
         int res = 0;
-        for (int c = 0; c < 26; c++) {
-            res = Math.max(res, dfs(N - 1, c, dp, s, k));
-        }
-        return res;
-    }
-
-    public int dfs(int i, int c, int[][] dp, String s, int k) {
-        // Memoized value
-        if (dp[i][c] != -1) {
-            return dp[i][c];
-        }
-
-        // State is not visited yet
-        dp[i][c] = 0;
-        boolean match = c == (s.charAt(i) - 'a');
-        if (match) {
-            dp[i][c] = 1;
-        }
-
-        // Non base case handling
-        if (i > 0) {
-            dp[i][c] = dfs(i - 1, c, dp, s, k);
-            if (match) {
-                for (int p = 0; p < 26; p++) {
-                    if (Math.abs(c - p) <= k) {
-                        dp[i][c] = Math.max(dp[i][c], dfs(i - 1, p, dp, s, k) + 1);
-                    }
+        // Updating dp with the i-th character
+        for (int i = 0; i < N; i++) {
+            int curr = s.charAt(i) - 'a';
+            int best = 0;
+            for (int prev = 0; prev < 26; prev++) {
+                if (Math.abs(prev - curr) <= k) {
+                    best = Math.max(best, dp[prev]);
                 }
             }
+
+            // Appending s[i] to the previous longest ideal subsequence allowed
+            dp[curr] = Math.max(dp[curr], best + 1);
+            res = Math.max(res, dp[curr]);
         }
-        return dp[i][c];
+        return res;
     }
 }
