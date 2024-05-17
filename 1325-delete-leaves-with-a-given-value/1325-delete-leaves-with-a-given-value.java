4b471ded421243b04e95467d55273eac86455d57
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-05-17 21:31:01 +0530

    Time: 3 ms (10.09%), Space: 44.4 MB (15.88%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -34,29 +34,26 @@ class Solution {
             stack.pop();
 
             if (currentNode.right == null && currentNode.left == null && currentNode.val == target) {
-                // If the stack is empty after popping, it means the root was a target leaf node.
                 if (stack.isEmpty()) {
-                    return null; // The tree becomes empty as the root itself is removed.
+                    return null; 
                 }
                 
                 // Identify the parent of the current node.
                 TreeNode parent = stack.isEmpty() ? null : stack.peek();
 
-                // Disconnect the current node from its parent.
                 if (parent != null) {
                     if (parent.left == currentNode) {
-                        parent.left = null; // If current is a left child, set the left pointer to null.
+                        parent.left = null; 
                     } else {
-                        parent.right = null; // If current is a right child, set the right pointer to null.
+                        parent.right = null; 
                     }
                 }
             }
 
-            // Mark this node as visited by setting 'lastRightNode' to 'currentNode' before moving to the next iteration.
             lastRightNode = currentNode;
-            // Reset 'currentNode' to null to ensure the next node from the stack is processed.
+
             currentNode = null;
         }
-        return root; // Return the modified tree.
+        return root; 
     }
 }
