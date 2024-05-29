987277362be531cdcce71c3ec17d6e14a97c7dcb
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-05-29 18:22:58 +0530

    Time: 1 ms (38.99%), Space: 41.1 MB (70.44%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,31 +1,21 @@
-class Solution {
+public class Solution {
+
     public int numSteps(String s) {
-        int steps = 0;
-        StringBuilder sb = new StringBuilder(s);
-        
-        while (sb.length() > 1) {
-            steps++;
-            if (sb.charAt(sb.length() - 1) == '0') {
-                // Even number: remove the last character (divide by 2)
-                sb.deleteCharAt(sb.length() - 1);
+        int N = s.length();
+
+        int operations = 0;
+        int carry = 0;
+        for (int i = N - 1; i > 0; i--) {
+            int digit = Character.getNumericValue(s.charAt(i)) + carry;
+
+            if (digit % 2 == 1) {
+                operations += 2;
+                carry = 1;
             } else {
-                // Odd number: add 1 to the binary number
-                int carry = 1;
-                for (int i = sb.length() - 1; i >= 0; i--) {
-                    if (sb.charAt(i) == '0') {
-                        sb.setCharAt(i, '1');
-                        carry = 0;
-                        break;
-                    } else {
-                        sb.setCharAt(i, '0');
-                    }
-                }
-                if (carry == 1) {
-                    sb.insert(0, '1');
-                }
+                operations++;
             }
         }
-        
-        return steps;
+
+        return operations + carry;
     }
 }
