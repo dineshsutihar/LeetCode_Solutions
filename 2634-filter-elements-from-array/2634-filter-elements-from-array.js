49c721224efc91cf8f7ad0f6ecd9aab533731a0b
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-06-18 16:32:01 +0530

    Time: 54 ms (51.31%), Space: 48.8 MB (61.06%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn)
+    var filteredArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
 };
