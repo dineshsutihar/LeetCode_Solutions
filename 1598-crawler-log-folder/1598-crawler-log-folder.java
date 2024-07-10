b77229b8e5843656bba2092018d6a0f122b88460
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-07-10 21:53:35 +0530

    Time: 1 ms (99.42%), Space: 41.7 MB (81.60%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,19 +1,14 @@
 class Solution {
     public int minOperations(String[] logs) {
         int currentStatus = 0; 
-        int leng = logs.length; 
-        
         for(String log: logs){
-           if(log.charAt(log.length()-2)=='.' && log.length()>2 && log.charAt(log.length()-3)=='.' && currentStatus != 0){
-               
+           if(log.length()>2 && log.charAt(log.length()-3)=='.' && currentStatus != 0){
                     currentStatus--;
            }
             if(log.charAt(log.length()-2)!='.'){
                 currentStatus++;
-            }
-            
+            }  
         }
-        
         return currentStatus; 
     
     }
