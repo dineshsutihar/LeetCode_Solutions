d8560ac06085e2d5c66d5f7afe10bb8dd70596f4
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-07-16 21:58:52 +0530

    Time: 25 ms (62.50%), Space: 78.6 MB (80.65%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -14,21 +14,21 @@
  * }
  */
 class Solution {
-    public TreeNode lca(TreeNode root, int p, int q){
-        if(root==null || root.val ==p || root.val ==q ){
-            return root; 
+//     public TreeNode lca(TreeNode root, int p, int q){
+//         if(root==null || root.val ==p || root.val ==q ){
+//             return root; 
             
-        }
-        TreeNode left = lca(root.left, p, q);
-        TreeNode right = lca(root.right, p, q);
-        if(left == null){
-            return right; 
-        }else if(right == null){
-            return left;
-        }else{
-            return root; 
-        }
-    }
+//         }
+//         TreeNode left = lca(root.left, p, q);
+//         TreeNode right = lca(root.right, p, q);
+//         if(left == null){
+//             return right; 
+//         }else if(right == null){
+//             return left;
+//         }else{
+//             return root; 
+//         }
+//     }
     
     public boolean findPath(TreeNode root, int target,  StringBuilder path){
         if(root == null){
@@ -43,7 +43,7 @@ class Solution {
             return true; 
         }
         
-        //backtracking
+        //backtracking 
         path.setLength(path.length()-1);
         path.append('R');
         if(findPath(root.right, target, path)){
@@ -56,17 +56,30 @@ class Solution {
         
     }
     public String getDirections(TreeNode root, int startValue, int destValue) {
-        TreeNode lca = lca(root, startValue, destValue);
+        // TreeNode lca = lca(root, startValue, destValue);
         StringBuilder sourceLca = new StringBuilder();
         StringBuilder lcaDest = new StringBuilder();
-        findPath(lca, startValue, sourceLca);        
-        findPath(lca, destValue, lcaDest);
+        findPath(root, startValue, sourceLca);        
+        findPath(root, destValue, lcaDest);
     
+        int p1 = 0; 
+        int p2 = 0; 
+        while(p1< sourceLca.length() && p2<lcaDest.length()){
+            if(sourceLca.charAt(p1) == lcaDest.charAt(p2)){
+                p1++; 
+                p2++;
+            }else{
+                break;
+            }
+        }
+        
         StringBuilder res = new StringBuilder();
-        for(int i =0; i<sourceLca.length();i++){
+        for(int i =p1; i<sourceLca.length();i++){
             res.append("U");
         }
-        res.append(lcaDest);
+         for(int i =p2; i<lcaDest.length();i++){
+            res.append(lcaDest.charAt(i));
+        }
         
         return res.toString(); 
     }
