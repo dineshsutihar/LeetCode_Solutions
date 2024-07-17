a7cc08edc14fa7932079dc0ec347127e1670bf79
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-07-17 22:48:59 +0530

    Time: 2 ms (27.75%), Space: 44.6 MB (58.32%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        Set<Integer> toDeleteSet = new HashSet<>();
+        for (int val : to_delete) {
+            toDeleteSet.add(val);
+        }
+
+        List<TreeNode> forest = new ArrayList<>();
+
+        Queue<TreeNode> nodesQueue = new LinkedList<>();
+        nodesQueue.add(root);
+
+        while (!nodesQueue.isEmpty()) {
+            TreeNode currentNode = nodesQueue.poll();
+
+            if (currentNode.left != null) {
+                nodesQueue.add(currentNode.left);
+                // Disconnect the left child if it needs to be deleted
+                if (toDeleteSet.contains(currentNode.left.val)) {
+                    currentNode.left = null;
+                }
+            }
+
+            if (currentNode.right != null) {
+                nodesQueue.add(currentNode.right);
+                // Disconnect the right child if it needs to be deleted
+                if (toDeleteSet.contains(currentNode.right.val)) {
+                    currentNode.right = null;
+                }
+            }
+
+            // If the current node needs to be deleted, add its non-null children to the forest
+            if (toDeleteSet.contains(currentNode.val)) {
+                if (currentNode.left != null) {
+                    forest.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    forest.add(currentNode.right);
+                }
+            }
+        }
+
+        // Ensure the root is added to the forest if it is not to be deleted
+        if (!toDeleteSet.contains(root.val)) {
+            forest.add(root);
+        }
+
+        return forest;
+    }
+}
