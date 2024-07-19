85eb01c2b4f1912ab87d99c36a7c2e411fd51873
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-07-19 14:32:05 +0530

    Time: 3 ms (42.69%), Space: 45.3 MB (32.97%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
@@ -1,36 +1,27 @@
 class Solution {
     public List<Integer> luckyNumbers (int[][] matrix) {
         List<Integer> result = new ArrayList<>();
-        // int[] colArr = new int[matrix.length];
-        // int[] rowArr = new int[matrix[0].length];
         Set<Integer> element = new HashSet<>();
         
         int incre =0; 
-        //Calculate row and Columns
         for(int[] row: matrix){
             int rowCurrent = Integer.MAX_VALUE; 
             for(int val: row){
                 rowCurrent = Math.min(rowCurrent,val); 
             }
-            // rowArr[incre++]=rowCurrent;
             element.add(rowCurrent);
         }
         
-        // Finding all Max Value in Col
         incre =0; 
         for(int i =0; i<matrix[0].length; i++ ){
             int colCurrent = Integer.MIN_VALUE; 
             for(int j=0; j<matrix.length;j++){
                 colCurrent = Math.max(colCurrent,matrix[j][i]); 
             }
-            // colArr[incre++]=colCurrent;
             if(element.contains(colCurrent)){
                 result.add(colCurrent);
             }
         }
-        //Row Col both done now its time for finding the Common on Both; 
-        
-        
         return result; 
     }
 }
