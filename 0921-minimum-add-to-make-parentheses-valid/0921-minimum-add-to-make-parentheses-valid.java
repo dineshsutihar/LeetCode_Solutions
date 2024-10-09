fe6e737f5a0ecbfbd58b36f52adb64e77cac152d
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-10-09 22:14:27 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (45.45%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,18 +1,39 @@
-class Solution {
-    public int minAddToMakeValid(String s) {
-        Stack<Character> stack = new Stack<>(); 
-        for(int i =0; i<s.length(); i++){
-            if(stack.isEmpty()){
-                stack.push(s.charAt(i)); 
-            }
-            else if(s.charAt(i)==')' && stack.peek()=='('){
-                stack.pop(); 
-            }
-            else{
-                stack.push(s.charAt(i)); 
+// class Solution {
+//     public int minAddToMakeValid(String s) {
+//         Stack<Character> stack = new Stack<>(); 
+//         for(int i =0; i<s.length(); i++){
+//             if(stack.isEmpty()){
+//                 stack.push(s.charAt(i)); 
+//             }
+//             else if(s.charAt(i)==')' && stack.peek()=='('){
+//                 stack.pop(); 
+//             }
+//             else{
+//                 stack.push(s.charAt(i)); 
+//             }
+//         }
+        
+//         return stack.size(); 
+//     }
+// }
+
+
+class Solution{
+    public int minAddToMakeValid(String s){
+        int openBrack = 0; 
+        int minRequired = 0; 
+        
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                openBrack++; 
+            }else {
+                if(openBrack >0){
+                    openBrack--; 
+                }else{
+                    minRequired++; 
+                }
             }
         }
-        
-        return stack.size(); 
+        return minRequired + openBrack; 
     }
 }
