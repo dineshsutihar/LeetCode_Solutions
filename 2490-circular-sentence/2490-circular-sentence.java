90ef5d886dcf146e28597b19de5e52211c3e898a
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-11-02 17:47:56 +0530

    Time: 1 ms (96.81%), Space: 41.5 MB (95.01%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        if(sentence.charAt(0)!=sentence.charAt(sentence.length()-1)) return false; 
+        
+        for(int i =1; i<sentence.length()-1; i++){
+            if(sentence.charAt(i)==' '){
+                if(sentence.charAt(i-1) != sentence.charAt(i+1)){
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+}
