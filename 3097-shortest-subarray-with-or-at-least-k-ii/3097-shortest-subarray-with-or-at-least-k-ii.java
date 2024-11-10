56a0a5d590b8bb65f5b11c587e9cd200c95012d8
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-11-10 21:53:20 +0530

    Time: 55 ms (68.89%), Space: 70 MB (41.07%) - LeetHub

---
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public int minimumSubarrayLength(int[] nums, int k) {
+        int minLength = Integer.MAX_VALUE;
+        int windowStart = 0;
+        int windowEnd = 0;
+        int[] bitCounts = new int[32];
+
+        while (windowEnd < nums.length) {
+            updateBitCounts(bitCounts, nums[windowEnd], 1);
+            while (
+                windowStart <= windowEnd &&
+                convertBitCountsToNumber(bitCounts) >= k
+            ) {
+                minLength = Math.min(minLength, windowEnd - windowStart + 1);
+
+                updateBitCounts(bitCounts, nums[windowStart], -1);
+                windowStart++;
+            }
+
+            windowEnd++;
+        }
+
+        return minLength == Integer.MAX_VALUE ? -1 : minLength;
+    }
+
+    private void updateBitCounts(int[] bitCounts, int number, int delta) {
+        for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
+            // Check if bit is set at current position
+            if (((number >> bitPosition) & 1) != 0) {
+                bitCounts[bitPosition] += delta;
+            }
+        }
+    }
+    private int convertBitCountsToNumber(int[] bitCounts) {
+        int result = 0;
+        for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
+            if (bitCounts[bitPosition] != 0) {
+                result |= 1 << bitPosition;
+            }
+        }
+        return result;
+    }
+}
