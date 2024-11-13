f79c66ae760f1faa72a620d8f6a3ce1522270975
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-11-13 21:11:28 +0530

    Time: 24 ms (81.88%), Space: 57.1 MB (80.94%) - LeetHub

---
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return lower_bound(nums, upper + 1) - lower_bound(nums, lower);
+    }
+
+    private long lower_bound(int[] nums, int value) {
+        int left = 0, right = nums.length - 1;
+        long result = 0;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < value) {
+                result += (right - left);
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
