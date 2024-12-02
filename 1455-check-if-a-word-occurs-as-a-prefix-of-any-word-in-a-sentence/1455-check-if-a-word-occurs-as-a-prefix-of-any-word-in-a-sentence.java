dccaae491210594f393a8bfa1c3fc37056b94a95
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-12-02 22:13:24 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (55.77%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (
+                words[i].length() >= searchWord.length() &&
+                words[i].substring(0, searchWord.length()).equals(searchWord)
+            ) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
