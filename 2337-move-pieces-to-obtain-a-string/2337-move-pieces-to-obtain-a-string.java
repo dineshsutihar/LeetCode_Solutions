a77275eed0730788c859cee3e2ed09aff4c72907
Dinesh Kumar Sutihar <110833165+dineshsutihar@users.noreply.github.com>
2024-12-05 20:59:11 +0530

    Time: 13 ms (92.35%), Space: 46 MB (18.75%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
@@ -12,18 +12,15 @@ public class Solution {
             ) {
                 startIndex++;
             }
-            // Skip underscores in target
             while (
                 targetIndex < startLength && target.charAt(targetIndex) == '_'
             ) {
                 targetIndex++;
             }
-            // If one string is exhausted, both should be exhausted
             if (startIndex == startLength || targetIndex == startLength) {
                 return startIndex == startLength && targetIndex == startLength;
             }
 
-            // Check if the pieces match and follow movement rules
             if (
                 start.charAt(startIndex) != target.charAt(targetIndex) ||
                 (start.charAt(startIndex) == 'L' && startIndex < targetIndex) ||
@@ -34,7 +31,6 @@ public class Solution {
             targetIndex++;
         }
 
-        // If all conditions are satisfied, return true
         return true;
     }
 }
